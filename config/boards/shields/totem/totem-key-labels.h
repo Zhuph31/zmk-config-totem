f8ba55c7be2fb70d3313818e5d34c1b109c6db3d
phzhu <phzhu99@gmail.com>
2025-09-14 15:11:43 -0400

    add labels

---
+++ config/boards/shields/totem/totem-key-labels.h
@@ -0,0 +1,53 @@
+#pragma once
+
+// Top row left
+#define LT0 RC(0,0)
+#define LT1 RC(0,1)
+#define LT2 RC(0,2)
+#define LT3 RC(0,3)
+#define LT4 RC(0,4)
+
+// Top row right
+#define RT0 RC(0,5)
+#define RT1 RC(0,6)
+#define RT2 RC(0,7)
+#define RT3 RC(0,8)
+#define RT4 RC(0,9)
+
+// Home row left
+#define LM0 RC(1,0)
+#define LM1 RC(1,1)
+#define LM2 RC(1,2)
+#define LM3 RC(1,3)
+#define LM4 RC(1,4)
+
+// Home row right
+#define RM0 RC(1,5)
+#define RM1 RC(1,6)
+#define RM2 RC(1,7)
+#define RM3 RC(1,8)
+#define RM4 RC(1,9)
+
+// Bottom row left
+#define PK0 RC(2,0)
+#define LB0 RC(3,0)
+#define LB1 RC(2,1)
+#define LB2 RC(2,2)
+#define LB3 RC(2,3)
+#define LB4 RC(2,4)
+
+// Bottom row right
+#define RB0 RC(2,5)
+#define RB1 RC(2,6)
+#define RB2 RC(2,7)
+#define RB3 RC(2,8)
+#define RB4 RC(2,9)
+#define PK1 RC(3,9)
+
+// Thumb cluster
+#define LH0 RC(3,2)
+#define LH1 RC(3,3)
+#define LH2 RC(3,4)
+#define RH0 RC(3,5)
+#define RH1 RC(3,6)
+#define RH2 RC(3,7)
